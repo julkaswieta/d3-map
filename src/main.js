338e2e2c7511c64cedfb5f0896b865e8513d5e4c
Julia Swietochowska <julkaswietochowska@gmail.com>
2023-10-24 16:53:21 +0100

    basic interactive map with coffee production data

--- src/main.js
+++ src/main.js
@@ -8,40 +8,56 @@ const svg = d3.select("body").append("svg")
     .attr("width", constants.SVG_WIDTH)
     .attr("height", constants.SVG_HEIGHT);
 
-const chart = svg.append("g").classed("chart", true);
+const chart = svg.append("g").classed("chart", true).attr("fill", "black");
 
 // this is what takes the coordinates of the countries borders and translates it onto a 2d plane using different cartographic methods
 const projection = d3.geoNaturalEarth1()
     .translate([constants.SVG_WIDTH / 2, constants.SVG_HEIGHT / 2]);
 
-console.log(projection.scale())
 const pathGenerator = d3.geoPath().projection(projection); // this is creating paths (country shapes) from coordinates using the selected projection  
 
-d3.json("../data/countries-50m.json").then(data => processData(data));
+d3.csv("../data/production.csv").then(function (coffeeData) {
+    color.domain([
+        d3.min(coffeeData, d => d["2019/20"]),
+        d3.max(coffeeData, d => d["2019/20"])
+    ]);
 
-const data = [{ name: "Poland", amount: 2000 },
-{ name: "Russia", amount: 4000 }];
+    d3.json("../data/countries-50m.json").then(function (geoData) {
+        const countries = feature(geoData, geoData.objects.countries).features;
+        // merge the dataset into the geojson as we need a single dataset to bind
+        for (let i = 0; i < coffeeData.length; i++) {
+            let coffeeCountry = coffeeData[i].Country;
+            let coffeeAmount = parseFloat(coffeeData[i]["2019/20"]);
+            //console.log(coffeeCountry + " " + coffeeAmount);
 
-const valueMap = new Map(data.map(d => [d.name, d.amount]));
+            for (var j = 0; j < countries.length; j++) {
+                let geoCountry = countries[j].properties.name;
+                if (coffeeCountry == geoCountry) {
+                    countries[j].properties.value = coffeeAmount;
+                    break;
+                }
 
-console.log(valueMap);
+            }
+        }
+        // get the value for a specific country 
+        //console.log(countries.filter(d => d.properties.name == "Zimbabwe")[0].properties.value);
 
-svg.call(d3.zoom()
-    .on("zoom", handleZoom));
-
-function processData(data) {
-    const countries = feature(data, data.objects.countries); // convert topojson to geojson
-    console.log(countries);
-    chart.selectAll("path")
-        .data(countries.features) // that's the data points
-        .join("path") // create a new path for each country
-        .classed("country", true)
-        .attr("d", pathGenerator) // that's the actual coordinates of the path 
-        .attr("fill", d => color(valueMap.get(d.properties.name)) ?? "blue")
-        // alternatively, can say d => geoGenerator(d)  
-        .append("title").text(d => d.properties.name);
-}
+        chart.selectAll("path")
+            .data(countries) // that's the data points
+            .join("path") // create a new path for each country
+            .classed("country", true)
+            .attr("d", pathGenerator) // that's the actual coordinates of the path 
+            .attr("fill", d => color(d.properties.value) ?? "#ccc")
+            .attr("stroke", "black")
+            // alternatively, can say d => geoGenerator(d)  
+            .append("title").text(d => d.properties.name + " " + d.properties.value);
+    })
+});
 
 function handleZoom(event) {
     chart.attr("transform", event.transform);
+}
+
+function getCountryNames(features) {
+    return features.map(d => d.properties.name)
 }
