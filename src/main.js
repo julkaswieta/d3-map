890f925f124bf06b828cc9388528817654a3923d
Julia Swietochowska <julkaswietochowska@gmail.com>
2024-02-09 17:49:44 +0000

    add slider (no onchange method yet)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import * as d3 from "d3";
 import { feature } from "topojson"
 import * as constants from "./constants.js"
+import { sliderBottom } from "d3-simple-slider";
 
-const color = d3.scaleQuantize([1, 10], d3.schemeBlues[9]);
+const color = d3.scaleLinear([1, 10], d3.schemeBlues[9]);
 
-const svg = d3.select("body").append("svg")
+const svg = d3.select("#app").append("svg")
     .attr("width", constants.SVG_WIDTH)
     .attr("height", constants.SVG_HEIGHT);
 
@@ -12,13 +13,41 @@ const chart = svg.append("g").classed("chart", true);
 // this is what takes the coordinates of the countries borders and translates it onto a 2d plane using different cartographic methods
 const projection = d3.geoNaturalEarth1();
 
-const pathGenerator = d3.geoPath().projection(projection); // this is creating paths (country shapes) from coordinates using the selected projection  
+const pathGenerator = d3.geoPath().projection(projection); // this is creating paths (country shapes) from coordinates using the selected projection
+
+const slider = sliderBottom();
+
+const sliderBox = d3.select('#slider')
+    .append('svg')
+    .attr('width', 1100)
+    .attr('height', 100)
+    .append('g')
+    .attr('transform', 'translate(30,30)');
 
 d3.csv("../data/production.csv").then(function (coffeeData) {
     color.domain([
         d3.min(coffeeData, d => d["2019/20"]),
         d3.max(coffeeData, d => d["2019/20"])
     ]);
+
+    let values = coffeeData.map(value => value["2019/20"]);
+    console.log(values);
+    coffeeData.columns.shift(); // get the columns and remove the first one ("Country")
+    let years = coffeeData.columns;
+    console.log(coffeeData.columns);
+
+    let yearsNumbers = years.map(year => +year.slice(0, 4))
+    console.log(yearsNumbers);
+
+    slider.min(d3.min(yearsNumbers))
+        .max(d3.max(yearsNumbers))
+        .tickValues(yearsNumbers)
+        .step(1)
+        .width(1000)
+        .tickFormat(d3.format("")) // removes the comma in thousands
+        .displayValue(true);
+
+    sliderBox.call(slider);
 
     d3.json("../data/countries-110m.json").then(function (geoData) {
         const geoJson = feature(geoData, geoData.objects.countries);
