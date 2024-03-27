0b54d456e46fa8e277d866109c6436e6125ea0e3
Julia Swietochowska <julkaswietochowska@gmail.com>
2024-03-27 20:59:32 +0000

    refactor unit tests

--- tests/linechart.tests.js
+++ tests/linechart.tests.js
@@ -7,7 +7,7 @@ import {
     getDatasetMinMax,
     getMinMax
 } from "../src/linechart";
-import { addDataset, clearDatasets, removeDataset } from "../src/datasets";
+import { addDataset, clearDatasets } from "../src/datasets";
 
 const countryMock1 = {
     properties: {
@@ -47,7 +47,6 @@ const consMinMax = { minYear: 1990, maxYear: 2019, maxValue: 18292011333 };
 
 const years = [1990, 1991, 1992, 1993, 1994, 1995, 2000, 2019];
 const prodValues = [3878394, 811919, 890222, 843272, 15278329, 18292011, 6738201, 673892];
-const consValues = [6478239, 5674839, 647389, 478390, 6478932, 18292011333, 6738201, 673892];
 const importValues = [56478, 567890, 345678];
 const importYears = [2000, 2001, 2020];
 
@@ -58,23 +57,37 @@ const importYearsValues = [
 ];
 
 describe("linechart helpers suite", () => {
-    test("extract years from country data", () => {
+    test("extract years from country data - standard dataset", () => {
         expect(extractYears("production", countryMock1)).toStrictEqual(years);
-        expect(extractYears("consumption", countryMock1)).toStrictEqual(years);
+    });
+
+    test("extract years from country data - non-existant dataset", () => {
         expect(extractYears("test-ds", countryMock1)).toStrictEqual([]);
+    });
+
+    test("extract years from country data - empty dataset", () => {
         expect(extractYears("testdataset", countryMock1)).toStrictEqual([]);
     });
 
-    test("extract values from country data", () => {
+
+    test("extract values from country data - standard dataset", () => {
         expect(extractValues("production", countryMock1)).toStrictEqual(prodValues);
-        expect(extractValues("consumption", countryMock1)).toStrictEqual(consValues);
+    });
+
+    test("extract values from country data - non-existant dataset", () => {
         expect(extractValues("test-ds", countryMock1)).toStrictEqual([]);
+    });
+
+    test("extract values from country data - empty dataset", () => {
         expect(extractValues("testdataset", countryMock1)).toStrictEqual([]);
     });
 
-    test("get dataset min max from country data", () => {
+    test("get dataset min max from country data - standard dataset", () => {
         expect(getDatasetMinMax("production", countryMock1))
             .toStrictEqual(prodMinMax);
+    });
+
+    test("get dataset min max from country data - empty dataset", () => {
         expect(getDatasetMinMax("testdataset", countryMock1))
             .toStrictEqual(testdsMinMax);
     });
@@ -109,12 +122,20 @@ describe("datasets-dependent linechart helpers suite", () => {
             .toStrictEqual(testdsMinMax);
     });
 
-    test("get min max of a country", () => {
+    test("get min max of a country - one dataset", () => {
+        addDataset("production");
+        expect(getMinMax(true, false, countryMock1)).toStrictEqual(prodMinMax);
+    });
+
+    test("get min max of a country - two datasets", () => {
         addDataset("production");
         expect(getMinMax(true, false, countryMock1)).toStrictEqual(prodMinMax);
         addDataset("consumption");
         expect(getMinMax(true, true, countryMock1)).toStrictEqual(consMinMax);
-        removeDataset("consumption");
+    });
+
+    test("get min max of a country - two datasets different years", () => {
+        addDataset("production");
         addDataset("import");
         expect(getMinMax(true, true, countryMock1)).toStrictEqual({
             minYear: 1990,
@@ -123,4 +144,3 @@ describe("datasets-dependent linechart helpers suite", () => {
         });
     });
 });
-
